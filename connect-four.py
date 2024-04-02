4adacb2240b22d58856ba421bf10ec7df28f41d3
David Steedman <dave@voxace.com>
2024-04-02 03:59:54 +0000

    Started connect four

--- connect-four.py
+++ connect-four.py
@@ -1,5 +1,25 @@
 def main():
-    # Your code goes here
+    board = initialiseBoard()
+    displayBoard(board)
+
+    # Red = 1, Yellow = 2
+    currentPlayer = 1
+    
+def initialiseBoard():
+    board = []
+    for _ in range(6):
+        row = []
+        for _ in range(7):
+            row.append('[]')
+        board.append(row)
+    return board
+    
+def displayBoard(board):
+    for row in board:
+        for cell in row:
+            print(cell, end=' ')
+        print()
+    print()
 
 if __name__ == "__main__":
     main()
